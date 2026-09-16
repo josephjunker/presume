c42e8c6dea02380574328ee84c0df01f431fc6f4
Joseph Junker <josephsoftwarequalityjunker@gmail.com>
2026-09-16 08:08:59 -0700

    rework stats collectors

--- lib/src/main/java/tech/jnkr/presume/StatsCollector.java
+++ lib/src/main/java/tech/jnkr/presume/StatsCollector.java
@@ -7,7 +7,7 @@ import java.util.function.Function;
 
 public class StatsCollector<T> {
     private final AbstractGenerator<T> generator;
-    private final ArrayList<EnumBucketer<T, ?>> bucketers;
+    private final ArrayList<BaseBucketer<T>> bucketers;
 
     public StatsCollector(AbstractGenerator<T> generator) {
         this.generator = generator;
@@ -20,11 +20,6 @@ public class StatsCollector<T> {
         this.bucketers.add(bucketer);
 
         return bucketer;
-    }
-
-    public enum BooleanEnum {
-        TRUE,
-        FALSE
     }
 
     public BooleanBucketer<T> addBooleanBucket(String title, Function<T, Boolean> discriminator) {
@@ -42,98 +37,53 @@ public class StatsCollector<T> {
         }
 
         System.out.println("STATS\n=====\n");
-        bucketers.forEach(EnumBucketer::print);
+        bucketers.forEach(BaseBucketer::print);
 
-        bucketers.forEach(EnumBucketer::validate);
+        bucketers.forEach(BaseBucketer::validate);
     }
 
-    public static class BooleanBucketer<T> extends EnumBucketer<T, BooleanEnum> {
-        private BooleanBucketer(String title, Function<T, Boolean> discriminator) {
-            super(
-                    title,
-                    BooleanEnum.class,
-                    (T t) -> discriminator.apply(t) ? BooleanEnum.TRUE : BooleanEnum.FALSE);
-        }
+    private record BucketWithCount(String name, int count) {}
 
-        public BooleanBucketer<T> withMinimumTrueRatio(float ratio) {
-            super.withMinimumRatio(BooleanEnum.TRUE, ratio);
-            return this;
-        }
+    private abstract static sealed class BaseBucketer<T>
+            permits BooleanBucketer, EnumBucketer, IntegerBucketer {
+        protected abstract void consume(T input);
 
-        public BooleanBucketer<T> withMaximumTrueRatio(float ratio) {
-            super.withMaximumRatio(BooleanEnum.TRUE, ratio);
-            return this;
-        }
+        abstract List<BucketWithCount> getBucketCounts();
 
-        public BooleanBucketer<T> withMinimumFalseRatio(float ratio) {
-            super.withMinimumRatio(BooleanEnum.FALSE, ratio);
-            return this;
-        }
+        protected abstract List<String> getBucketNames();
 
-        public BooleanBucketer<T> withMaximumFalseRatio(float ratio) {
-            super.withMaximumRatio(BooleanEnum.FALSE, ratio);
-            return this;
-        }
-    }
+        protected abstract int getCountForBucket(String name);
 
-    public static class EnumBucketer<T, Buckets extends Enum<Buckets>> {
-        private final String title;
-        private final HashMap<Buckets, Integer> buckets;
-        private final Function<T, Buckets> discriminator;
-        private final Buckets[] bucketInEnumOrder;
-        private final HashMap<Buckets, Float> minimumRatioAssertions;
-        private final HashMap<Buckets, Float> maximumRatioAssertions;
+        protected abstract String getTitle();
 
-        protected EnumBucketer(
-                String title, Class<Buckets> bucketsEnum, Function<T, Buckets> discriminator) {
-            this.title = title;
-            buckets = new HashMap<>();
-            this.discriminator = discriminator;
-            bucketInEnumOrder = bucketsEnum.getEnumConstants();
+        protected final Map<String, Float> minimumRatioAssertions = new HashMap<>();
+        protected final Map<String, Float> maximumRatioAssertions = new HashMap<>();
 
-            for (Buckets bucket : bucketsEnum.getEnumConstants()) {
-                buckets.put(bucket, 0);
-            }
-
-            minimumRatioAssertions = new HashMap<>();
-            maximumRatioAssertions = new HashMap<>();
-        }
-
-        private void consume(T t) {
-            Buckets bucket = discriminator.apply(t);
-            increment(bucket);
-        }
-
-        private void increment(Buckets bucket) {
-            buckets.put(bucket, buckets.get(bucket) + 1);
-        }
-
-        private void print() {
-            ArrayList<Map.Entry<Buckets, Integer>> entries = new ArrayList<>(buckets.entrySet());
+        protected void print() {
+            List<BucketWithCount> entries = getBucketCounts();
 
             int maxLabelLength =
                     entries.stream()
-                            .map(entry -> entry.getKey().name().length())
+                            .map(entry -> entry.name().length())
                             .max(Comparator.naturalOrder())
                             .orElse(0);
 
-            int totalEntries = entries.stream().mapToInt(Map.Entry::getValue).sum();
+            int totalEntries = entries.stream().mapToInt(BucketWithCount::count).sum();
 
             int leftMargin = maxLabelLength + 3;
 
-            System.out.println(title);
-            for (int i = 0; i < title.length(); i++) {
+            System.out.println(getTitle());
+            for (int i = 0; i < getTitle().length(); i++) {
                 System.out.print("-");
             }
             System.out.println();
-            for (Buckets bucket : bucketInEnumOrder) {
-                int value = buckets.get(bucket);
-                String name = bucket.name();
+            for (String bucketName : getBucketNames()) {
+                int value = getCountForBucket(bucketName);
 
-                int padding = leftMargin - name.length();
+                int padding = leftMargin - bucketName.length();
                 int percent = (int) ((float) value * 100 / (float) totalEntries);
 
-                System.out.print(name);
+                System.out.print(bucketName);
                 for (int i = 0; i < padding; i++) {
                     System.out.print(".");
                 }
@@ -143,31 +93,225 @@ public class StatsCollector<T> {
             System.out.println();
         }
 
-        private void validate() {
-            float totalEntries = buckets.values().stream().mapToInt(i -> i).sum();
+        protected void validate() {
+            float totalEntries = getBucketCounts().stream().mapToInt(BucketWithCount::count).sum();
 
             for (var entry : minimumRatioAssertions.entrySet()) {
-                float value = buckets.get(entry.getKey());
+                float value = getCountForBucket(entry.getKey());
                 float actualRatio = value / totalEntries;
                 float expectedRatio = entry.getValue();
                 if (actualRatio < expectedRatio)
                     throw GeneratorStatsAssertionFailureException.minimumFailed(
-                            title, entry.getKey().name(), expectedRatio, actualRatio);
+                            getTitle(), entry.getKey(), expectedRatio, actualRatio);
             }
 
             for (var entry : maximumRatioAssertions.entrySet()) {
-                float value = buckets.get(entry.getKey());
+                float value = getCountForBucket(entry.getKey());
                 float actualRatio = value / totalEntries;
                 float expectedRatio = entry.getValue();
                 if (actualRatio > expectedRatio)
                     throw GeneratorStatsAssertionFailureException.maximumFailed(
-                            title, entry.getKey().name(), expectedRatio, actualRatio);
+                            getTitle(), entry.getKey(), expectedRatio, actualRatio);
             }
+        }
+    }
+
+    public static final class IntegerBucketer extends BaseBucketer<Integer> {
+        private final String title;
+        private final int bucketCount;
+        private final int bucketSize;
+        private final int min;
+        private final HashMap<String, Integer> counts;
+
+        private IntegerBucketer(String title, int min, int max, int bucketCount) {
+            super();
+            this.title = title;
+            this.bucketCount = bucketCount;
+            this.min = min;
+            bucketSize = (max - min) / bucketCount;
+
+            counts = new HashMap<>();
+        }
+
+        private String getBucketName(int index) {
+            int bucketMin = index * bucketSize + min;
+            int bucketMax = bucketMin + bucketSize;
+            return String.format("%d to %d", bucketMin, bucketMax);
+        }
+
+        @Override
+        protected void consume(Integer input) {
+            int index = (input - min) / bucketSize;
+            String bucketName = getBucketName(index);
+            counts.compute(bucketName, (name, value) -> Objects.isNull(value) ? 1 : value + 1);
+        }
+
+        @Override
+        List<BucketWithCount> getBucketCounts() {
+            ArrayList<BucketWithCount> result = new ArrayList<>();
+            for (int i = 0; i < bucketCount; i++) {
+                String bucketName = getBucketName(i);
+                result.add(new BucketWithCount(bucketName, counts.getOrDefault(bucketName, 0)));
+            }
+            return result;
+        }
+
+        @Override
+        protected List<String> getBucketNames() {
+            ArrayList<String> result = new ArrayList<>();
+            for (int i = 0; i < bucketCount; i++) {
+                result.add(getBucketName(i));
+            }
+            return result;
+        }
+
+        @Override
+        protected int getCountForBucket(String name) {
+            return counts.getOrDefault(name, 0);
+        }
+
+        @Override
+        protected String getTitle() {
+            return title;
+        }
+    }
+
+    public static final class BooleanBucketer<T> extends BaseBucketer<T> {
+        private final String title;
+        private final Function<T, Boolean> discriminator;
+        private int trueCount = 0;
+        private int falseCount = 0;
+
+        private BooleanBucketer(String title, Function<T, Boolean> discriminator) {
+            this.title = title;
+            this.discriminator = discriminator;
+        }
+
+        public BooleanBucketer<T> withMinimumTrueRatio(float ratio) {
+            minimumRatioAssertions.compute(
+                    "TRUE",
+                    (key, existing) ->
+                            Objects.isNull(existing) ? ratio : Math.min(existing, ratio));
+            return this;
+        }
+
+        public BooleanBucketer<T> withMaximumTrueRatio(float ratio) {
+            maximumRatioAssertions.compute(
+                    "TRUE",
+                    (key, existing) ->
+                            Objects.isNull(existing) ? ratio : Math.min(existing, ratio));
+            return this;
+        }
+
+        public BooleanBucketer<T> withMinimumFalseRatio(float ratio) {
+            minimumRatioAssertions.compute(
+                    "FALSE",
+                    (key, existing) ->
+                            Objects.isNull(existing) ? ratio : Math.min(existing, ratio));
+            return this;
+        }
+
+        public BooleanBucketer<T> withMaximumFalseRatio(float ratio) {
+            minimumRatioAssertions.compute(
+                    "FALSE",
+                    (key, existing) ->
+                            Objects.isNull(existing) ? ratio : Math.min(existing, ratio));
+            return this;
+        }
+
+        @Override
+        protected void consume(T input) {
+            if (discriminator.apply(input)) {
+                trueCount++;
+            } else {
+                falseCount++;
+            }
+        }
+
+        @Override
+        List<BucketWithCount> getBucketCounts() {
+            return List.of(
+                    new BucketWithCount("TRUE", trueCount),
+                    new BucketWithCount("FALSE", falseCount));
+        }
+
+        @Override
+        protected List<String> getBucketNames() {
+            return List.of("TRUE", "FALSE");
+        }
+
+        @Override
+        protected int getCountForBucket(String name) {
+            if (name.equals("TRUE")) return trueCount;
+            if (name.equals("FALSE")) return falseCount;
+            throw new IllegalArgumentException("TILT: Tried to get bad name from boolean bucket");
+        }
+
+        @Override
+        protected String getTitle() {
+            return title;
+        }
+    }
+
+    public static final class EnumBucketer<T, Buckets extends Enum<Buckets>>
+            extends BaseBucketer<T> {
+        private final String title;
+        private final HashMap<Buckets, Integer> buckets;
+        private final Function<T, Buckets> discriminator;
+        private final Buckets[] bucketInEnumOrder;
+        private final Class<Buckets> bucketsEnum;
+
+        EnumBucketer(String title, Class<Buckets> bucketsEnum, Function<T, Buckets> discriminator) {
+            super();
+            this.title = title;
+            buckets = new HashMap<>();
+            this.discriminator = discriminator;
+            bucketInEnumOrder = bucketsEnum.getEnumConstants();
+            this.bucketsEnum = bucketsEnum;
+
+            for (Buckets bucket : bucketsEnum.getEnumConstants()) {
+                buckets.put(bucket, 0);
+            }
+        }
+
+        @Override
+        List<BucketWithCount> getBucketCounts() {
+            return new ArrayList<>(buckets.entrySet())
+                    .stream()
+                            .map(
+                                    entry ->
+                                            new BucketWithCount(
+                                                    entry.getKey().name(), entry.getValue()))
+                            .toList();
+        }
+
+        @Override
+        protected List<String> getBucketNames() {
+            return Arrays.stream(bucketInEnumOrder).map(Enum::name).toList();
+        }
+
+        @Override
+        protected int getCountForBucket(String name) {
+            return buckets.get(Enum.valueOf(bucketsEnum, name));
+        }
+
+        @Override
+        protected String getTitle() {
+            return title;
+        }
+
+        protected void consume(T t) {
+            Buckets bucket = discriminator.apply(t);
+            increment(bucket);
+        }
+
+        private void increment(Buckets bucket) {
+            buckets.put(bucket, buckets.get(bucket) + 1);
         }
 
         public EnumBucketer<T, Buckets> withMinimumRatio(Buckets bucket, float ratio) {
             minimumRatioAssertions.compute(
-                    bucket,
+                    bucket.name(),
                     (key, existing) ->
                             Objects.isNull(existing) ? ratio : Math.max(existing, ratio));
 
@@ -176,7 +320,7 @@ public class StatsCollector<T> {
 
         public EnumBucketer<T, Buckets> withMaximumRatio(Buckets bucket, float ratio) {
             maximumRatioAssertions.compute(
-                    bucket,
+                    bucket.name(),
                     (key, existing) ->
                             Objects.isNull(existing) ? ratio : Math.min(existing, ratio));
 
--- lib/src/test/java/tech/jnkr/presume/IntegerGeneratorTests.java
+++ lib/src/test/java/tech/jnkr/presume/IntegerGeneratorTests.java
@@ -1,33 +1,92 @@
 package tech.jnkr.presume;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import tech.jnkr.presume.internal.atoms.AtomSource;
 import tech.jnkr.presume.internal.atoms.DrawAtom;
 import tech.jnkr.presume.internal.atoms.Regular;
 
 public class IntegerGeneratorTests {
-    @RepeatedTest(10)
-    public void shouldProduceBothOddAndEvenNumbers() {
-        IntegerGenerator generator = new IntegerGenerator(IntegerGeneratorTests::getRegular);
+    @ParameterizedTest
+    @CsvSource({
+        "10, 0, 0",
+        "0, 0, 0",
+        "100, 0, 0",
+        "4096, 0, 0",
+        "9999999, 0, 0",
+        "0, -100, 0",
+        "100, 33, 0",
+        "4096, 1024, 0",
+        "9999999, 777, 0",
+        "0, -100, 0",
+        "100, -33, 0",
+        "4096, -1024, 0",
+        "9999999, -777, 0",
+        "10, 0, 5",
+        "0, 0, 99999",
+        "100, 0, 40",
+        "4096, 0, 3333",
+        "9999999, 0, 1",
+        "0, -100, -25",
+        "100, 33, 35",
+        "4096, 1024, 2048",
+        "9999999, 777, 33333",
+        "0, -100, -98",
+        "100, -33, -11",
+        "4096, -1024, 1000",
+        "9999999, -777, 10000",
+    })
+    public void shouldProduceBothOddAndEvenNumbers(int max, int min, int shrinkTowards) {
+        IntegerGenerator gen = new IntegerGenerator(IntegerGeneratorTests::getRegular);
+        if (max != 0) gen = gen.withMaximum(max);
+        if (min != 0) gen = gen.withMinimum(min);
+        if (shrinkTowards != 0) gen = gen.shrinkingTowards(shrinkTowards);
 
-        int evenCount = 0;
-        int oddCount = 0;
-        for (int i = 0; i < 1000; i++) {
-            if (generator.gen(new PureSource()) % 2 == 0) {
-                evenCount++;
-            } else {
-                oddCount++;
-            }
-        }
+        StatsCollector<Integer> collector = new StatsCollector<>(gen);
+        collector
+                .addBooleanBucket("wasEven", n -> n % 2 == 0)
+                .withMinimumTrueRatio(0.3f)
+                .withMinimumFalseRatio(0.3f);
 
-        assertTrue(evenCount > 250);
-        assertTrue(oddCount > 250);
+        collector.summarize(10_000);
     }
 
-    public void shouldProduceRelativelyEvenDistribution() {}
+    @ParameterizedTest
+    @CsvSource({
+        "10, 0, 0",
+        "0, 0, 0",
+        "100, 0, 0",
+        "4096, 0, 0",
+        "9999999, 0, 0",
+        "0, -100, 0",
+        "100, 33, 0",
+        "4096, 1024, 0",
+        "9999999, 777, 0",
+        "0, -100, 0",
+        "100, -33, 0",
+        "4096, -1024, 0",
+        "9999999, -777, 0",
+        "10, 0, 5",
+        "0, 0, 99999",
+        "100, 0, 40",
+        "4096, 0, 3333",
+        "9999999, 0, 1",
+        "0, -100, -25",
+        "100, 33, 35",
+        "4096, 1024, 2048",
+        "9999999, 777, 33333",
+        "0, -100, -98",
+        "100, -33, -11",
+        "4096, -1024, 1000",
+        "9999999, -777, 10000",
+    })
+    public void shouldProduceRelativelyEvenDistribution(int min, int max, int shrinkTowards) {
+        IntegerGenerator gen = new IntegerGenerator(IntegerGeneratorTests::getRegular);
+        if (max != 0) gen = gen.withMaximum(max);
+        if (min != 0) gen = gen.withMinimum(min);
+        if (shrinkTowards != 0) gen = gen.shrinkingTowards(shrinkTowards);
+    }
 
     public static DrawAtom getRegular() {
         AtomSource source = new AtomSource();
